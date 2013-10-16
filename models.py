289bb8bc0b3e1fa96f71135401a425524b867a80
Christopher Groskopf <staringmonkey@gmail.com>
2013-10-16 10:38:47 -0500

    Filter out non-legislative officials. Closes #55.

--- models.py
+++ models.py
@@ -46,11 +46,16 @@ def load_data():
     """
     Load database tables from files.
     """
+    SKIP_TYPES = ['Local Government Official', 'Public Official', 'ATTORNEY GENERAL', 'STATE TREASURER']
+
     with open('data/sample_data.csv') as f:
         reader = csvkit.CSVKitDictReader(f)
         rows = list(reader)
 
     i = 0
+    expenditures = []
+    warnings = []
+    errors = []
 
     for row in rows:
         for k, v in row.items():
@@ -63,17 +68,23 @@ def load_data():
         if recipient_type in ['Senator', 'Representative']:
             recipient_official = recipient
             recipient_official_type = recipient_type
-        else:
+        elif recipient_type in ['Employee or Staff', 'Spouse or Child']:
             # TODO
             recipient_official = ''
             recipient_official_type = ''
+        elif recipient_type in SKIP_TYPES:
+            warnings.append('%i -- Skipping %s: %s' % (i, recipient_type, recipient))
+            continue
+        else:
+            errors.append('%i -- Unknown recipient type, "%s": %s' % (i, recipient_type, recipient))
+            continue
 
         bits = map(int, row['Date'].split('/'))
         event_date = datetime.date(bits[2], bits[0], bits[1])
 
         cost = float(row['Cost'].strip('()').strip('$'))
 
-        Expenditure.create(
+        expenditures.append(Expenditure(
             lobbyist_name='%(Lob F Name)s %(Lob L Name)s' % row,
             lobbyist_last_name=row['Lob L Name'],
             report_period=report_period,
@@ -86,8 +97,27 @@ def load_data():
             description=row['Description'],
             cost=cost,
             principal=row['Principal']
-        )
+        ))
 
         i += 1
+
+    if warnings:
+        print 'WARNINGS'
+        print '--------'
+
+        for warning in warnings:
+            print warning
+
+    if errors:
+        print 'ERRORS'
+        print '------'
+
+        for error in errors:
+            print error
+
+        return
+
+    for expenditure in expenditures:
+        expenditure.save()
 
     print 'Imported %i expenditures' % i
