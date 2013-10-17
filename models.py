1a6d4dc70c25cf19d2d78ae4469a57481e5a65fb
Christopher Groskopf <staringmonkey@gmail.com>
2013-10-17 11:17:04 -0500

    Add group data.

--- models.py
+++ models.py
@@ -104,6 +104,7 @@ class Expenditure(Model):
     description =  CharField()
     cost = FloatField()
     organization = ForeignKeyField(Organization, related_name='expenditures')
+    group = ForeignKeyField(Group, related_name='expenditures', null=True)
     
     class Meta:
         database = database
@@ -112,7 +113,7 @@ def delete_tables():
     """
     Clear data from sqlite.
     """
-    for cls in [Lobbyist, Legislator, Organization, Expenditure]:
+    for cls in [Group, Lobbyist, Legislator, Organization, Expenditure]:
         try:
             cls.drop_table()
         except:
@@ -122,10 +123,13 @@ def create_tables():
     """
     Create database tables for each model.
     """
-    for cls in [Lobbyist, Legislator, Organization, Expenditure]:
+    for cls in [Group, Lobbyist, Legislator, Organization, Expenditure]:
         cls.create_table()
 
 class LobbyLoader:
+    """
+    Load expenditures from files.
+    """
     SKIP_TYPES = ['Local Government Official', 'Public Official', 'ATTORNEY GENERAL', 'STATE TREASURER', 'GOVERNOR', 'STATE AUDITOR', 'LIEUTENANT GOVERNOR', 'SECRETARY OF STATE', 'JUDGE']
 
     party_lookup = {}
@@ -134,15 +138,35 @@ class LobbyLoader:
     warnings = []
     errors = []
 
-    row_index = 0
+    individual_rows = 0
+    group_rows = 0
     lobbyists_created = 0
     legislators_created = 0
     organizations_created = 0
+    groups_created = 0
 
     def __init__(self):
         self.party_lookup_filename = 'data/party_lookup.csv'
         self.individual_data_filename = 'data/sample_data.csv'
         self.group_data_filename = 'data/sample_group_data.csv'
+
+    def warn(self, msg):
+        self.warnings.append(msg)
+
+    def error(self, msg):
+        self.errors.append(msg)
+
+    def load_party_lookup(self):
+        """
+        Load lobbyist->party mapping from file.
+        """
+        with open(self.party_lookup_filename) as f:
+            reader = csvkit.CSVKitReader(f, encoding='latin1')
+            
+            for row in reader:
+                recipient = tuple(map(unicode.strip, row[0].rsplit(' - ', 1)))
+
+                self.party_lookup[recipient] = row[1]
 
     def load_lobbyist(self, name):
         """
@@ -199,28 +223,36 @@ class LobbyLoader:
 
         return True, organization
 
-    def load_expenditures(self):
+    def load_group(self, name):
         """
-        Load database tables from files.
+        Get or create a group.
         """
-        # These offices will be skipped
+        try:
+            return False, Group.get(Group.name==name)
+        except Group.DoesNotExist:
+            pass
 
-        # Load parties
-        with open(self.party_lookup_filename) as f:
-            reader = csvkit.CSVKitReader(f, encoding='latin1')
-            
-            for row in reader:
-                recipient = tuple(map(unicode.strip, row[0].rsplit(' - ', 1)))
+        group = Group(
+            name=name
+        )
 
-                self.party_lookup[recipient] = row[1]
+        group.save()
 
+        return True, group
+
+    def load_individual_expenditures(self):
+        """
+        Load individual expenditures from files.
+        """
         # Load data
         with open(self.individual_data_filename) as f:
-            reader = csvkit.CSVKitDictReader(f, encoding='latin1')
+            reader = csvkit.CSVKitDictReader(f)
             rows = list(reader)
 
+        i = 0
+
         for row in rows:
-            self.row_index += 1
+            i += 1
 
             # Strip whitespace
             for k, v in row.items():
@@ -246,27 +278,27 @@ class LobbyLoader:
                 party = self.party_lookup.get((recipient, recipient_type), '')
 
                 if not party:
-                    self.errors.append('%05i -- No matching party affiliation for "%s": "%s"' % (self.row_index, recipient_type, recipient))
+                    self.error('%05i -- No matching party affiliation for "%s": "%s"' % (i, recipient_type, recipient))
 
                 created, legislator = self.load_legislator(recipient, recipient_type, party)
             elif recipient_type in ['Employee or Staff', 'Spouse or Child']:
                 legislator_name, legislator_type = map(unicode.strip, row['Pub Off'].rsplit(' - ', 1))
 
                 if legislator_type in self.SKIP_TYPES:
-                    self.warnings.append('%05i -- Skipping "%s": "%s" for "%s": "%s"' % (self.row_index, recipient_type, recipient, legislator_type, legislator_name))
+                    self.warn('%05i -- Skipping "%s": "%s" for "%s": "%s"' % (i, recipient_type, recipient, legislator_type, legislator_name))
                     continue
 
                 party = self.party_lookup.get((legislator_name, legislator_type), '')
 
                 if not party:
-                    self.errors.append('%05i -- No matching party affiliation for "%s": "%s"' % (self.row_index, legislator_name, legislator_type))
+                    self.error('%05i -- No matching party affiliation for "%s": "%s"' % (i, legislator_name, legislator_type))
 
                 created, legislator = self.load_legislator(legislator_name, legislator_type, party)
             elif recipient_type in self.SKIP_TYPES:
-                self.warnings.append('%05i -- Skipping "%s": "%s"' % (self.row_index, recipient_type, recipient))
+                self.warn('%05i -- Skipping "%s": "%s"' % (i, recipient_type, recipient))
                 continue
             else:
-                self.errors.append('%05i -- Unknown recipient type, "%s": "%s"' % (self.row_index, recipient_type, recipient))
+                self.error('%05i -- Unknown recipient type, "%s": "%s"' % (i, recipient_type, recipient))
                 continue
 
             if created:
@@ -280,7 +312,7 @@ class LobbyLoader:
             cost = row['Cost'].strip('$').replace(',', '')
 
             if '(' in cost or '-' in cost:
-                self.errors.append('%05i -- Negative cost!' % self.row_index)
+                self.error('%05i -- Negative cost!' % i)
                 continue
 
             cost = float(cost)
@@ -302,14 +334,88 @@ class LobbyLoader:
                 category=row['Type'],
                 description=row['Description'],
                 cost=cost,
-                organization=organization
+                organization=organization,
+                group=None
             ))
+
+        self.individual_rows = i 
+
+    def load_group_expenditures(self):
+        """
+        Load group expenditures from files.
+        """
+        # Load data
+        with open(self.group_data_filename) as f:
+            reader = csvkit.CSVKitDictReader(f)
+            rows = list(reader)
+
+        i = 0
+
+        for row in rows:
+            i += 1
+
+            # Strip whitespace
+            for k, v in row.items():
+                row[k] = v.strip()
+
+            # Lobbyist
+            created, lobbyist = self.load_lobbyist('%s %s' % (row['Lob F Name'], row['Lob L Name']))
+
+            if created:
+                self.lobbyists_created += 1 
+
+            # Report period
+            report_period = datetime.datetime.strptime(row['Report'], '%b-%y').date()
+
+            # Group
+            created, group = self.load_group(row['Group'])
+
+            if created:
+                self.groups_created += 1
+
+            # Event date
+            bits = map(int, row['Date'].split('/'))
+            event_date = datetime.date(bits[2], bits[0], bits[1])
+
+            # Cost
+            cost = row['Cost'].strip('$').replace(',', '')
+
+            if '(' in cost or '-' in cost:
+                self.error('%05i -- Negative cost!' % i)
+                continue
+
+            cost = float(cost)
+
+            # Organization
+            created, organization = self.load_organization(row['Principal'])
+
+            if created:
+                self.organizations_created += 1
+
+            # Create it!
+            self.expenditures.append(Expenditure(
+                lobbyist=lobbyist,
+                report_period=report_period,
+                recipient='',
+                recipient_type='',
+                legislator=None,
+                event_date=event_date,
+                category=row['Type'],
+                description=row['Description'],
+                cost=cost,
+                organization=organization,
+                group=group
+            ))
+
+        self.group_rows = i
 
     def run(self):
         """
         Run the loader and output summary.
         """
-        self.load_expenditures()
+        self.load_party_lookup()
+        self.load_individual_expenditures()
+        self.load_group_expenditures()
 
         if self.warnings:
             print 'WARNINGS'
@@ -336,7 +442,8 @@ class LobbyLoader:
         print 'SUMMARY'
         print '-------'
 
-        print 'Processed %i rows' % self.row_index 
+        print 'Processed %i individual rows' % self.individual_rows
+        print 'Processed %i group rows' % self.group_rows 
         print 'Imported %i expenditures' % len(self.expenditures)
         print 'Created %i lobbyists' % self.lobbyists_created
         print 'Created %i legislators' % self.legislators_created
